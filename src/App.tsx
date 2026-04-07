9561288ef9ffac5de1d34e685ff69acc360aea67
Tomy <tomyaryafiosa@gmail.com>
2026-04-07 19:59:13 +0700

    implement admin login/logout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Dashboard from "./pages/admin/Dashboard"
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+
+        {/* Admin Routes */}
+        <Route path="/admin" element={<Dashboard />} />
       </Routes>
     </Router>
   )
